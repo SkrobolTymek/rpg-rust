bf32d3ce9c2df6fa4524a765d2cdffd2a67e2f1c
SkrobolTymek <Tymoteusz.skrobol@gmail.com>
2025-03-22 12:27:03 +0100

    implementacja itemow, do poprawy, dalej niektore itemy nie dzialaja

--- dungeon-explorer/src/main.rs
+++ dungeon-explorer/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use rand::Rng;
 
+#[derive(Clone)]
 struct Player {
     health: i32,
     strength: i32,
@@ -46,6 +47,7 @@ fn main() {
     ];
 
     let mut rng = rand::thread_rng();
+    let mut traci_ture = false;
 
     while saizzan.health > 0 {
         let random_index = rng.gen_range(0..monsters.len());
@@ -100,18 +102,24 @@ fn main() {
                         }
                         
                     
-                        saizzan.ekwipunek.push(zdobyta_nagroda);
-                        println!("Twoje nagrody: {:?}", saizzan.ekwipunek);
+                        saizzan.ekwipunek.push(zdobyta_nagroda.clone());
+                        println!("ZNALAZLES: {}", zdobyta_nagroda);
                         break;
                     } else {
+                        if traci_ture ==false{
                         println!("Potwór atakuje!");
                         saizzan.health -= chosen_monster.strength;
                         println!("Twoje HP: {}", saizzan.health);
-
                         if saizzan.health <= 0 {
                             println!("Dildodini penisini, tralalelo tralala, umarłeś.");
                             return;
                         }
+                    }
+                    else {
+                        println!("Potwor nie ma tury");
+                        traci_ture = false;
+                    }
+                       
                     }
                 }
                 "2" => {
@@ -132,6 +140,52 @@ fn main() {
                         saizzan.gold -= 15;
                     } else {
                         println!("Nie stać Cię XDDD");
+                    }
+                }
+                "4" => {
+                    if saizzan.ekwipunek.is_empty() {
+                        println!("Twój ekwipunek jest pusty.");
+                    } else {
+                        println!("Twój ekwipunek:");
+                        for (i, item) in saizzan.ekwipunek.iter().enumerate() {
+                            println!("{} - {}", i + 1, item);
+                        }
+
+                        let mut item_choice = String::new();
+                        io::stdin().read_line(&mut item_choice).expect("Błąd zczytywania");
+                        let item_choice: usize = item_choice.trim().parse().expect("Wybierz numer przedmiotu");
+
+                        if item_choice > 0 && item_choice <= saizzan.ekwipunek.len() {
+                            let item = saizzan.ekwipunek.remove(item_choice - 1);
+
+                            match item.as_str() {
+                                "Jabol Smocza Krew – Regeneruje pełne HP" => {
+                                    saizzan.health = saizzan.max_health;
+                                    println!("Użyłeś Jabol Smocza Krew, pełne HP!");
+                                }
+                                "Eliksir Czteropaka – Podwaja twoją siłę" => {
+                                    saizzan.strength *= 2;
+                                    println!("Użyłeś Eliksiru Czteropaka, twoja siła została podwojona!");
+                                }
+                                "Zbutwiały Sandał – Potwór traci turę" => {
+                                    println!("Użyłeś Zbutwiały Sandał, potwór traci turę!");
+                                    traci_ture = true;
+                                }
+                                "Łzawa Poezja Emo – Potwór dostaje depresji" => {
+                                    println!("Użyłeś Łzawą Poezję Emo, potwór popełnia samobójstwo!");
+                                    chosen_monster.health = 0;
+                                }
+                                "Krysztalowa kula - instakill przeciwnika" => {
+                                    println!("Użyłeś Krysztalową kulę, potwór umiera natychmiast!");
+                                    chosen_monster.health = 0;
+                                }
+                                _ => {
+                                    println!("Nieznany przedmiot");
+                                }
+                            }
+                        } else {
+                            println!("Zły wybór przedmiotu");
+                        }
                     }
                 }
                 "9" => {
